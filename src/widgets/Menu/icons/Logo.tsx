aa1be04ed2fa7b1ceda676ceefef745917bddcf8
thefishadmin <lrdeveloperconsole@gmail.com>
2021-03-03 14:52:58 +0530

    Fix logo

--- src/widgets/Menu/icons/Logo.tsx
+++ src/widgets/Menu/icons/Logo.tsx
@@ -9,10 +9,11 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 205 26" {...props}>
-      <image width="205" height="26" href={isDark ? '/images/egg/LogoTextNewDark.png' : '/images/egg/LogoTextNewWhite.png'}/>
-    </Svg>
-  )
+    <image
+      className="fishLogo"
+      href={isDark ? "/images/egg/LogoTextNewDark.png" : "/images/egg/LogoTextNewWhite.png"}
+    />
+  );
 };
 
 export default Logo;
